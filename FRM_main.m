73c20bb7628db6ebe9289f21b1cd420150b470ec
Apple <apple@GJGJMEOWMIMEOW.local>
2017-05-11 23:47:28 +0800

    comments on the main program

---
+++ FRM_main.m
@@ -0,0 +1,20 @@
+%THIS IS A COMMENT
+
+%Input morphology here
+
+clock = 0;%clock time initialization
+%need some variables/arrays to store data
+
+
+
+
+
+%calculate t for each possible pathway, keeping the smallest time
+
+%execute the process with the smallest t, advance clock by t, and update
+%the system.
+
+
+
+
+%repeat
